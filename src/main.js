0cab6c1b0dfaa68b7e84d1b9bbbefa4deb242047
Hazmi Hafiz <hazmihafiz@gmail.com>
2020-12-29 14:55:15 +0700

    feat: add pwa

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { defineCustomElements } from '@ionic/pwa-elements/loader'
 import 'modern-css-reset'
 import App from './App.vue'
+import './registerServiceWorker'
 
 createApp(App).mount('#app')
 defineCustomElements(window)
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pwa: {
+        name: 'Geeef',
+        short_name: 'Geeef',
+        themeColor: '#feac5e',
+        msTileColor: '#feac5e',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    },
+}
